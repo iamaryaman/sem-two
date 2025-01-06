9446584827da55f8c1a477eb9e8221220af76d69
iamaryaman <aryaroyal2514@gmail.com>
2025-01-07 05:16:18 +0530

    Rock paper scissors (turned 90 line code into 24)

---
+++ rps.py
@@ -0,0 +1,23 @@
+import random as r
+  
+def comp_choose():
+    return r.randint(1, 3)
+
+player1 = input("Player's choice - Type 1 for Rock, 2 for Paper, 3 for Scissors: ")
+
+choicedict = {1:"Rock",2:"Paper",3:"Scissors"}
+
+if player1.isdigit():
+    player1= int(player1)
+    player1 = choicedict[player1]
+comp1 = choicedict[comp_choose()]
+
+final = (player1,comp1)
+finalcheck = {"Computer Wins":[("Rock","Paper"),("Scissors","Rock"),("Paper","Scissors")],"Player Wins":[("Rock","Scissors"),("Scissors","Paper"),("Paper","Rock")]}
+
+for result, combination in finalcheck.items():
+    if final in combination:
+        print(result)
+        break
+else:
+    print("Draw")  
